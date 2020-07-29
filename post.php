ba0cb5bf8b472644f5a008de564912b80d1e7c4a
subashini <46003877+subasaranya@users.noreply.github.com>
2020-07-29 18:32:16 +0530

    Add files via upload

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php
+$name=$_POST[name]
+$lastname=$_POST[lastname]
+$mail=$_POST[email]
+$psw=$_POST[psw]
+echo "First Name:$name</br>
+Last Name:$lastname</br>
+Mail:$mail</br>
+password:$psw</br>"
+?>
